b60a94ce24ab04cdf281bae33d47a5558b7e4d90
melkilic <melike180400@gmail.com>
2020-03-30 14:41:22 -0500

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,16 @@
+require 'pry'
+class Dog
+attr_accessor :name, :breed, :age
+@@all = []
+    def initialize(name = nil, breed = nil, age = nil)
+        @name = name
+        @breed = breed
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+# binding.pry    
+end
